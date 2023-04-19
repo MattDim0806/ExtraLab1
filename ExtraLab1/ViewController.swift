9b0afcc5bceffa721319ea8f46e46c4d0133b777
Matt <yanghouling0806@gmail.com>
2023-04-19 20:13:10 +0800

    Initial Commit

---
+++ ExtraLab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExtraLab1
+//
+//  Created by 楊皓麟 on 2023/4/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
